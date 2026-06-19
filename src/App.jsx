ad15aca0aa5b48565527f050004c6465b9f6ddc8
Ademola <demisterling007@gmail.com>
2026-06-19 09:04:52 +0100

    first commit on steps-app

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+import "./App.css";
+function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [message, setMessage] = useState();
+  function handleNext() {
+    if (step < 3) {
+      setStep((prev) => prev + 1);
+    }
+    setCount(count + 1);
+  }
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((prev) => prev - 1);
+    }
+
+    setCount((prev) => prev - 1);
+  }
+
+  function handleTogle() {
+    setIsOpen((prev) => !prev);
+  }
+  return (
+    <>
+      {" "}
+      <button className="close" onClick={handleTogle}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}{" "}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>{" "}
+        </div>
+      )}{" "}
+    </>
+  );
+}
+export default App;
